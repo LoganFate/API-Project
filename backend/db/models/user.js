dc4f9bb4ecfb92942a763ebd4ca5b765d153c688
Logan Fate <logan.m.fate@gmail.com>
2023-12-20 17:39:32 -0600

    Add User authentication Express middleware

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,28 +1,52 @@
 'use strict';
-const { Model } = require('sequelize');
+const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {}
-
-  User.init({
-    username: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-      unique: true
-    },
-    email: {
-      type: DataTypes.STRING(256),
-      allowNull: false,
-      unique: true
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false
+  class User extends Model {
+    static associate(models) {
+      // define association here
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  };
 
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
+      }
+    }
+  );
   return User;
 };
