d0f5bbc1dc89df82ae6b678c979ed516bb10c94c
Logan Fate <logan.m.fate@gmail.com>
2023-12-22 12:50:25 -0600

    4th render attempt

--- backend/db/seeders/20231220230938-demo-user.js
+++ backend/db/seeders/20231220230938-demo-user.js
@@ -14,17 +14,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'random',
+        lastName: 'person'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'other',
+        lastName: 'user'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'last',
+        lastName: 'one'
       }
     ], { validate: true });
   },
