5abfa837a24514138ef50a05ae8cb1144f15a62b
Logan Fate <logan.m.fate@gmail.com>
2024-10-01 14:26:26 -0500

    Attempt 4

--- backend/db/migrations/20231222223107-create-spot.js
+++ backend/db/migrations/20231222223107-create-spot.js
@@ -57,7 +57,7 @@ module.exports = {
         allowNull: false
       },
       price: {
-        type: Sequelize.DECIMAL,
+        type: Sequelize.STRING,
         allowNull: false
       },
       createdAt: {
